b603b63c5eae17b8d849ab686ada1b5428fc77e1
Juan Henao <jhenao@truora.com>
2023-11-11 17:07:00 -0500

    Create initial project with FastAPI
    KAN-23

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+#from router import user
+
+# To migrate the predefined databases
+#from database.db import Base, engine
+#Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+#app.include_router(user.router, prefix="/users", tags=["User"])
+
+@app.get("/", tags=["Root"])
+def main():
+    return {"message": "Hello World"}
