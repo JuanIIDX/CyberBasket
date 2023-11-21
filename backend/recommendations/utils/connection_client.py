fb6bfc1ce37d9e8f5df5f80280508644225ce183
Luis Gonzalez <luchogo170@gmail.com>
2023-11-20 23:48:11 -0500

    Se agrego funcionalidad para cargar usuarios desde un archivo excel y eliminar todos los usuarios

--- backend/recommendations/utils/connection_client.py
+++ backend/recommendations/utils/connection_client.py
@@ -1,10 +1,15 @@
 from recombee_api_client.api_client import RecombeeClient
+from recombee_api_client.api_client import RecombeeClient, Region
 import os
+from dotenv import load_dotenv
+import os
+load_dotenv()
 def get_recombee_cliente():
     db_id = os.getenv('DATABASE_ID')
     private_token = os.getenv('PRIVATE_TOKEN')
-    return RecombeeClient(db_id, private_token)
-    
-    
+    print("Varibales de entorno")
+    print(db_id, private_token)
+
+    return RecombeeClient(db_id, private_token,region=Region.US_WEST)
     
     
