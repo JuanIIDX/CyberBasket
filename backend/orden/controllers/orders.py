4f515c268909a43db583d73931d3d4abbafbe33f
sebas2485 <sebastian.1701821561@ucaldas.edu.co>
2023-11-24 18:05:54 -0500

    CRUD ORDENES FUNCIONAL

--- backend/orden/controllers/orders.py
+++ backend/orden/controllers/orders.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
-from models.orders import Orden, Detalle_Orden
+from ..models.orders import Orden, Detalle_Orden
+from ..schemas.orders import OrderBase, OrderDetailBase
 
 def get_ordenes(db: Session):
     return db.query(Orden).all()
@@ -7,21 +8,29 @@ def get_ordenes(db: Session):
 def get_orden(db: Session, orden_id: int):
     return db.query(Orden).filter(Orden.id == orden_id).first()
 
-def create_orden(db: Session, orden: Orden):
-    db.add(orden)
+def create_orden(db: Session, orden: OrderBase):
+    orden_model = Orden(**orden.dict())
+    db.add(orden_model)
     db.commit()
-    db.refresh(orden)
-    return orden
+    db.refresh(orden_model)
+    return orden_model
 
-def update_orden(db: Session, orden: Orden):
-    db.merge(orden)
-    db.commit()
-    return orden
+def update_orden(db: Session, orden_id: int, orden: OrderBase):
+    db_orden = db.query(Orden).filter(Orden.id == orden_id).first()
+    if db_orden:
+        for key, value in orden.dict().items():
+            setattr(db_orden, key, value)
+        db.commit()
+    print(db_orden, type(orden))   
+    orden_update = {**db_orden.__dict__, **orden.dict()} 
+    return orden_update
 
 def delete_orden(db: Session, orden_id: int):
     orden = db.query(Orden).filter(Orden.id == orden_id).first()
     db.delete(orden)
     db.commit()
+
+#controladorres detalle orden
 
 def get_detalle_ordenes(db: Session):
     return db.query(Detalle_Orden).all()
--- backend/orden/models/orders.py
+++ backend/orden/models/orders.py
@@ -7,24 +7,27 @@ Base = declarative_base()
     
 class Orden(Base):
     __tablename__ = "orden"
-    id = Column('ID_OrdenPro', Integer, primary_key=True)
-    id_pago = Column('ID_Pago', Integer, ForeignKey('pago.ID_Pago'), nullable=False)
-    id_envio = Column('ID_Envio', Integer, ForeignKey('envio.ID_Envio'))
-    impuesto = Column('Impuesto', Float, nullable=False)
-    estado = Column('Estado', String(10), nullable=False)
-    fecha_creacion = Column('Fecha_Creacion', Date, nullable=False)
-    fecha_actualizacion = Column('Fecha_Actualizacion', Date, nullable=False)
-    pago = relationship('Pago')
-    envio = relationship('Envio')
-
+    id = Column('id', Integer, primary_key=True)
+    id_pago = Column('id_Pago', Integer,  nullable=True)
+    id_envio = Column('id_Envio', Integer, nullable=True)   
+    # id_pago = Column('id_Pago', Integer, ForeignKey('pago.ID_Pago'), nullable=False)
+    # id_envio = Column('id_Envio', Integer, ForeignKey('envio.ID_Envio'))
+    impuesto = Column('impuesto', Float, nullable=False)
+    estado = Column('estado', String(10), nullable=False)
+    fecha_creacion = Column('fecha_creacion', Date, nullable=False)
+    fecha_actualizacion = Column('fecha_actualizacion', Date, nullable=False)
+    # pago = relationship('Pago')
+    # envio = relationship('Envio')
+ #   detalle_ordenes = relationship('Detalle_Orden', back_populates='orden')
 
 class Detalle_Orden(Base):
-    __tablename__ = "detalle_orden"
-    id = Column('ID_DetalleOrd', Integer, primary_key=True)
-    orden_id = Column('ID_OrdenPro', Integer, ForeignKey('orden.ID_OrdenPro'))
-    producto_id = Column('ID_Producto', Integer, ForeignKey('producto.ID_Producto'))
+    __tablename__ = "Detalle_orden"
+    id = Column('id', Integer, primary_key=True)
+    orden_id = Column('orde_id', Integer, ForeignKey('orden.id'))
+    producto_id = Column('id_producto', Integer, ForeignKey('producto.id_producto'))
     cantidad = Column('Cantidad', Integer)
     precio_unitario = Column('Precio_Unitario', Float)
-    orden = relationship('Orden', back_populates='detalle_ordenes')
-    producto = relationship('Producto', back_populates='detalle_ordenes')
-    Orden.detalle_ordenes = relationship('Detalle_Orden', back_populates='orden')
+    #orden = relationship('Orden'), 
+    #producto = relationship('Producto')
+    
+#Orden.detalle_orden = relationship('Detalle_Orden', back_populates='orden')
