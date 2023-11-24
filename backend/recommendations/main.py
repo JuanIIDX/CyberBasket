99d84ad278ff35abcecf006571290e147501c68a
Luis Gonzalez <luchogo170@gmail.com>
2023-11-24 14:44:41 -0500

    Se ajustan las importaciones en el  ms-recommendations

--- backend/recommendations/main.py
+++ backend/recommendations/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from routes.recommendation import router
+from .routes.recommendation import router
 from dotenv import load_dotenv
-import os
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 load_dotenv()
 app = FastAPI()
 app.include_router( router, prefix='/recommendation', tags=['recomendation'])
--- backend/recommendations/services/recommendation.py
+++ backend/recommendations/services/recommendation.py
@@ -1,13 +1,26 @@
 # services/recombee_service.py
-from recombee_api_client.api_client import RecombeeClient
-from recombee_api_client.api_requests import RecommendItemsToUser
-from utils.connection_client import get_recombee_cliente
+from ..utils.connection_client import get_recombee_cliente
+from recombee_api_client.api_requests import AddDetailView, RecommendItemsToUser, SearchItems
+
 
 class RecombeeService:
     def __init__(self):
         self.client = get_recombee_cliente()
+        
+        
+    def add_detail_view(self, user_id: str, item_id: str):
+        request = AddDetailView(user_id, item_id, cascade_create=True)
+        self.client.send(request)
+        
 
     def get_recommendations(self, user_id: str, count: int):
-        request = RecommendItemsToUser(user_id, count)
+        request = RecommendItemsToUser(user_id, count,return_properties=True)
         response = self.client.send(request)
         return response
+    
+    def search_items(self, query: str, count: int):
+        request = SearchItems(query, count,return_properties=True)
+        response = self.client.send(request)
+        return response
+    
+   
