cd80c4d1a1cd1008e95938555173c177689ebcf0
Juan <juan.1701523251@ucaldas.edu.co>
2023-12-06 18:21:23 -0500

    Actualizacion de archivos de documentacion

--- backend/inventario/controllers/inventario_controller.py
+++ backend/inventario/controllers/inventario_controller.py
@@ -8,3 +8,22 @@ def exist_producto(producto_name: str, db):
 
 def all_productos(db):
     return db.query(Producto).all()
+
+def create_producto(new_producto: Crea_producto, db):
+
+    producto = Producto(**new_producto.dict())
+    db.add(producto)
+    db.commit()
+    db.refresh(producto)
+    return producto
+
+def get_productos_por_tienda(tienda_id: int,db):
+    result = (
+        db.query(Producto.nombre, Producto.descripcion, Inventario.cantidad)
+        .join(Inventario, Inventario.id_producto == Producto.id_producto)
+        .join(Tienda, Inventario.id_tienda == Tienda.id_tienda)
+        .filter(Tienda.id_tienda == tienda_id)
+        .all()
+    )
+    print(result)
+    return result
--- backend/inventario/routers/routers.py
+++ backend/inventario/routers/routers.py
@@ -16,4 +16,30 @@ router = APIRouter()
 def get_all_productos(db: Session = Depends(get_db)):
     return all_productos(db)
 
+# Ruta de ejemplo en FastAPI
+@router.get("/consulta/{tienda_id}",response_model=list[consulta_productos_por_tienda])
+async def get_consulta(tienda_id: int, db: Session = Depends(get_db)):
+    try:
+        # Realizar la consulta
+        result = get_productos_por_tienda(tienda_id,db)
+        # Convertir el resultado a JSON
+        """ data = [{"nombre": nombre, "descripcion": descripcion, "stock": stock} for nombre, descripcion, stock in result]
+        return {"data": data} """
+        return result
+    except Exception as e:
+        print("Esta incorrecto")
+    finally:
+        db.close()
+
+#Agrega producto
+@router.post("/producto")
+def create_new_producto(new_producto: Crea_producto, db: Session = Depends(get_db)):
+    exist = exist_producto(new_producto.nombre, db)
+    if exist:
+        return {"message": "Producto already exist"}
+
+    rol = create_producto(new_producto, db)
+    return rol
+
+
 
