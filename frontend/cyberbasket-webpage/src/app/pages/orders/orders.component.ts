ef52333ec48425dc0b0442adff9b5852c3c7e8c3
Juan <juan.1701523251@ucaldas.edu.co>
2023-12-14 22:40:57 -0500

    test_subir_order

--- frontend/cyberbasket-webpage/src/app/pages/orders/orders.component.ts
+++ frontend/cyberbasket-webpage/src/app/pages/orders/orders.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./orders.component.scss']
 })
 export class OrdersComponent {
+  
+    constructor() { }
+  
+    ngOnInit(): void {
+    }
+
+/*     carga_datos_ordenes(): Promise<any> {
+      return new Promise((resolve) => {
+        this.inventarioServicio.carga_datos_producto(this.id_producto_pagina)
+          .subscribe(
+            (resp) => {
+              console.log('Valid response:', resp);
+              this.nombre_producto=resp.nombre_producto;
+              this.nombre_tienda=resp.nombre_tienda;
+              this.descripcion_producto=resp.descripcion;
+              this.stock=resp.cantidad;
+              this.precio_producto=resp.precio;
+
+              console.log("Se cargo correctamente la informacion del producto");
+              resolve(resp);
+            },
+            (err) => {
+              console.error('Error:', err);
+              resolve(null);
+            }
+          );
+      });
+    } */
 
 }
